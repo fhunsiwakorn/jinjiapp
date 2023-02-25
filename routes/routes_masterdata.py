b0c45190c15056a0973fa3d9ce19ff3539dad82d
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-02-25 15:22:08 +0700

    Update routes_masterdata.py

--- routes/routes_masterdata.py
+++ routes/routes_masterdata.py
@@ -219,17 +219,17 @@ def update_skill(skill_id: int, request: SkillRequestInSchema,  db: Session = De
     return ResponseProcess(status="Ok", status_code="200", message="Success update data")
 
 
-# @router_masterdata.get("/skill/get/all", response_model=List[SkillFullRequestOutSchema])
-# def get_skill(db: Session = Depends(get_db), authenticated: bool = Depends(auth_request)):
-#     obj = []
-#     for rs in main_skill:
-#         main_skill_id = rs['main_skill_id']
-#         _skill = db.query(Skill).order_by(asc(Skill.skill_name)).filter(
-#             Skill.skill_group_type == main_skill_id, Skill.cancelled == 1).all()
-#         response = {'main_skill_id': main_skill_id,
-#                     'main_skill_name': rs['main_skill_name'], 'subskills': _skill}
-#         obj.append(response)
-#     return obj
+@router_masterdata.get("/skill/get/all", response_model=List[SkillFullRequestOutSchema])
+def get_skill(db: Session = Depends(get_db), authenticated: bool = Depends(auth_request)):
+    obj = []
+    for rs in main_skill:
+        main_skill_id = rs['main_skill_id']
+        _skill = db.query(Skill).order_by(asc(Skill.skill_name)).filter(
+            Skill.skill_group_type == main_skill_id, Skill.cancelled == 1).all()
+        response = {'main_skill_id': main_skill_id,
+                    'main_skill_name': rs['main_skill_name'], 'subskills': _skill}
+        obj.append(response)
+    return obj
 @router_masterdata.get("/skill", response_model=List[SkillRequestOutSchema])
 def get_skill_option(typeskill: int, db: Session = Depends(get_db), authenticated: bool = Depends(auth_request)):
     if typeskill > 0:
